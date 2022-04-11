21420a4535900392df38e3076c6995c565fe03e8
Nico.escobar <nic.escobar@duocuc.cl>
2022-04-11 11:35:49 -0400

    prueba2

---
+++ probando1/AgregarCita.js
@@ -0,0 +1,38 @@
+<form >
+      <div className="form-group row">
+          <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
+          <div className="col-sm-8 col-lg-10">
+              <input type="text" className="form-control" placeholder="Nombre Mascota" />
+          </div>
+      </div>
+      <div className="form-group row">
+          <label className="col-sm-4 col-lg-2 col-form-label">Nombre Dueño</label>
+          <div className="col-sm-8 col-lg-10">
+              <input type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" />
+          </div>
+      </div>
+
+       <div className="form-group row">
+          <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
+          <div className="col-sm-8 col-lg-4  mb-4 mb-lg-0">
+              <input type="date" className="form-control" />
+          </div>                            
+
+          <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
+          <div className="col-sm-8 col-lg-4">
+              <input type="time" className="form-control" />
+          </div>
+      </div>
+
+      <div className="form-group row">
+          <label className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
+          <div className="col-sm-8 col-lg-10">
+              <textarea  className="form-control"></textarea>
+          </div>
+      </div>
+      <div className="form-group row justify-content-end">
+          <div className="col-sm-3">
+              <button type="submit" className="btn btn-success w-100">Agregar</button>
+          </div>
+      </div>
+  </form>
