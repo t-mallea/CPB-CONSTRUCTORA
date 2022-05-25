f0c4a6f4d73f7fbc66f66747af59602c43fd6d51
oliver167 <ogonzalez@duocuc.cl>
2022-05-24 20:32:32 -0400

    cambio de color

--- styles/feriados.js
+++ styles/feriados.js
@@ -3,6 +3,8 @@
 let url = "https://apis.digital.gob.cl/fl/feriados"
 $.get(url, function(respuesta){
 
-    console.log(respuesta)
+    let feriado = respuesta[respuesta.lenght -1]
+
+    $("#feriado").text(feriado.nombre + " - " + feriado.fecha)
 
 }, "json")
