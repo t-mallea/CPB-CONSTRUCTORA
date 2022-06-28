2e4c91d40f65359187191fc921f2eda27ddb71ab
Nico.escobar <nic.escobar@duocuc.cl>
2022-06-27 22:53:20 -0400

    clases

--- api/models.py
+++ api/models.py
@@ -2,8 +2,7 @@ from typing_extensions import runtime
 from django.db import models
 
 # Create your models here.
-class Usuario (models.Model):
-    nombreCompleto = models.CharField(max_length=50)
-    rut = models.IntegerField(max_length=20)
-    correo = models.CharField(max_length=40)
-    contraseña = models.CharField(max_length=20)
+
+    
+    
+    
