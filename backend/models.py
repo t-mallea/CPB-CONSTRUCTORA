fc2288b4152cdceb2f069ea68fb4eab1afd009b9
Nico.escobar <nic.escobar@duocuc.cl>
2022-06-28 14:03:07 -0400

    class hospital

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from typing_extensions import runtime
-from django.db import models
-
-# Create your models here.
-class Usuario (models.Model):
-    nombreCompleto = models.CharField(max_length=50)
-    rut = models.IntegerField(max_length=20)
-    correo = models.CharField(max_length=40)
-    contraseña = models.CharField(max_length=20)
--- hospital/models.py
+++ hospital/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 # Create your models here.
-gp9999
+class Usuario (models.Model):
+    nombreCompleto = models.CharField(max_length=50)
+    rut = models.models.IntegerField(max_length=20)
+    correo = models.CharField(max_length=40)
+    contraseña = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nombreCompleto
+    
+class agendarHora (models.Model):
+    fecha = models.IntegerField(max_length=12)    
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    
+    def __str__(self): 
+        return self.fecha
